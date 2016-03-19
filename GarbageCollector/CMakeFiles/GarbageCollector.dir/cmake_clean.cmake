25f10bf85c399aee1a69125a89b026bb7ba8879d
Dar Mehta <dmehta97@gmail.com>
2016-03-19 14:04:10 -0400

    init commit including cmake and opencv setup

---
+++ GarbageCollector/CMakeFiles/GarbageCollector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GarbageCollector.dir/GarbageCollector.cpp.o"
+  "GarbageCollector.pdb"
+  "GarbageCollector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GarbageCollector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GarbageCollector/GarbageCollector.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  GarbageCollector
+//
+//  Created by Dar Mehta on 2016-03-19.
+//  Copyright © 2016 Dar Mehta. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/xfeatures2d/nonfree.hpp>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
